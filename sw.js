432eda7d5ee7df41b362c36c865d8da220bc437a
marceriscanoza-BNHS <marceris.canoza@deped.gov.ph>
2026-02-18 09:17:46 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'inventory-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://unpkg.com/dexie/dist/dexie.js',
+  'https://unpkg.com/html5-qrcode'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
